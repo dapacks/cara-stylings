a8119a16abd1b38cc5c2ff32f6b9b367d5042822
Deepak <60600822+dapacks@users.noreply.github.com>
2024-01-31 02:11:56 +0530

    Update items.js

--- 2-actual-backend/data/items.js
+++ 2-actual-backend/data/items.js
@@ -1,15 +1,22 @@
+const path = require('path');
 const fs = require('node:fs/promises');
 
+const filePath = path.join(__dirname, 'items.json');
+
 async function getStoredItems() {
-  const rawFileContent = await fs.readFile('items.json', { encoding: 'utf-8' });
+  const rawFileContent = await fs.readFile(filePath, { encoding: 'utf-8' });
   const data = JSON.parse(rawFileContent);
   const storedItems = data.items ?? [];
   return storedItems;
 }
 
 function storeItems(items) {
-  return fs.writeFile('items.json', JSON.stringify({ items: items || [] }));
+  return fs.writeFile(filePath, JSON.stringify({ items: items || [] }));
 }
+
+exports.getStoredItems = getStoredItems;
+exports.storeItems = storeItems;
+
 
 exports.getStoredItems = getStoredItems;
 exports.storeItems = storeItems;
